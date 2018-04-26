3616de81f235578767009ac775644c54202b10aa
Julian Zenker <j.zenker@gmx.net>
2018-04-26 17:11:37 +0200

    Changed paths to fit project structure

--- src/back end/msd_walker.py
+++ src/back end/msd_walker.py
@@ -8,29 +8,19 @@ import numpy as np
 from sklearn.cluster import KMeans
 import hdf5_getters as GETTERS
 
-msd_subset_path = 'C:/Users/jzenk/OneDrive/Dokumente/Uni HH BA SSE/S04 SS18 Projekt Wirtschaftsinformatik/MillionSongSubset'
-msd_subset_data_path = os.path.join(msd_subset_path, 'data')
-msd_subset_addf_path = os.path.join(msd_subset_path, 'AdditionalFiles')
+msd_path = '/msd/MillionSongSubset'
+msd_data_path = os.path.join(msd_path, 'data')
+msd_addf_path = os.path.join(msd_path, 'AdditionalFiles')
 
 song_infos = {}
 
 
 def test():
-    f = GETTERS.open_h5_file_read(
-        'C:/Users/jzenk/Documents/GitHub/music-recommender/src/back end/TRAAAAW128F429D538.h5')
-    X = np.array([[1, 2], [1, 4], [1, 0], [4, 2], [4, 4], [4, 0]])
-    kmeans = KMeans(n_clusters=2, random_state=0).fit(X)
-    print(kmeans.labels_)
-    print(kmeans.predict([[0, 0], [4, 4]]))
-    print(kmeans.cluster_centers_)
-    print(GETTERS.get_tempo(f))
-    print(GETTERS.get_danceability(f))
-    print(GETTERS.get_loudness(f))
-    return GETTERS.get_artist_name(f)
+    return 'Test'
 
 
 def read_song_infos():
-    for root, dirs, files in os.walk(msd_subset_data_path):
+    for root, dirs, files in os.walk(msd_data_path):
         files = glob.glob(os.path.join(root, '*'+'.h5'))
         for file in files:
             h5 = GETTERS.open_h5_file_read(file)
