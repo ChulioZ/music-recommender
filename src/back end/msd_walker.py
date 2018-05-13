9a4b1bf2dbcdbca987316cedc48b3e687109cdf3
Julian Zenker <j.zenker@gmx.net>
2018-05-13 10:40:26 +0200

    Removed fancyimpute

--- src/back end/msd_walker.py
+++ src/back end/msd_walker.py
@@ -10,7 +10,6 @@ from sklearn.cluster import KMeans
 import hdf5_getters as GETTERS
 import mysql.connector as mc
 import pandas as pd
-import fancyimpute
 from sklearn.preprocessing import Imputer
 
 file_path = os.path.dirname(os.path.dirname(
