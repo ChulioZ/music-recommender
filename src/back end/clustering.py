ddb3a396467bce0d1241d8b188b012e19bde144a
Julian Zenker <j.zenker@gmx.net>
2018-05-28 19:28:40 +0200

    finished clustering

--- src/back end/clustering.py
+++ src/back end/clustering.py
@@ -10,44 +10,56 @@ import pyodbc
 from os.path import dirname, abspath
 from mice import MICE
 
-server = 'mrd.database.windows.net'
-database = 'mrd'
-username = 'qaywsx'
-password = 'w@970881'
-connection = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER=' +
-                            server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD=' + password)
-cursor = connection.cursor()
-param = "SELECT loudness,hotttnesss,tempo,timeSig,songkey,mode FROM songs"
-cursor.execute(param)
-paramResults = cursor.fetchall()
-details = "SELECT title,artist FROM songs"
-cursor.execute(details)
-detailsResults = cursor.fetchall()
 
-song_array = np.array(paramResults)
+def do_kmeans():
+    start = time.time()
 
-print song_array
+    server = 'mrd.database.windows.net'
+    database = 'mrd'
+    username = 'qaywsx'
+    password = 'w@970881'
+    connection = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER=' +
+                                server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD=' + password)
+    cursor = connection.cursor()
 
+    print('Hole Parameter')
+    param = "SELECT loudness,hotttnesss,tempo,timeSig,songkey,mode FROM songs"
+    cursor.execute(param)
+    paramResults = cursor.fetchall()
+    song_array = np.array(paramResults)
+    print(str(datetime.timedelta(seconds=time.time()-start)))
 
+    print('Hole IDs')
+    ids = "SELECT id FROM songs"
+    cursor.execute(ids)
+    idsResults = cursor.fetchall()
+    id_array = np.array(idsResults)
+    print(id_array[3][0])
+    print(str(datetime.timedelta(seconds=time.time()-start)))
 
+    print('Führe k-means++ aus')
+    kmeans = KMeans(n_clusters=35, init='k-means++')
+    kmeans.fit(song_array)
+    print(str(datetime.timedelta(seconds=time.time()-start)))
 
-kmeans = KMeans(n_clusters=35, init='k-means++')
-kmeans.fit(song_array)
+    centroids = kmeans.cluster_centers_
+    format_strc = """INSERT INTO centroids (label, loudness, hotttnesss, tempo, timeSig, songkey, mode)
+                VALUES ({label}, {loudness}, {hotttnesss}, {tempo}, {timeSig}, {songkey}, {mode});"""
+    for i in range(0, len(centroids)):
+        sql_command = format_strc.format(label=i, loudness=centroids[i][0], hotttnesss=centroids[i][1],
+                                         tempo=centroids[i][2], timeSig=centroids[i][3], songkey=centroids[i][4], mode=centroids[i][5])
+        cursor.execute(sql_command)
+        print('centroid'+str(i)+'eingetragen')
+    print(str(datetime.timedelta(seconds=time.time()-start)))
 
-centroids = kmeans.cluster_centers_
-labels = kmeans.labels_
+    labels = kmeans.labels_
+    format_strl = """UPDATE songs SET label = {label} WHERE id = '{id}';"""
+    for i in range(0, len(labels)):
+        sql_command = format_strl.format(label=labels[i], id=id_array[i][0])
+        cursor.execute(sql_command)
+        print('label'+str(i)+'eingetragen')
 
-print(centroids)
-print(labels)
-    
-    
-for i in labels:
-        format_str = """INSERT INTO songs (label)
-        VALUES ({label});"""
-        sql_command = format_str.format(label = labels[i])
-        cursor.execute(sql_command)    
-
-
-connection.commit()
-cursor.close()
-connection.close()
+    connection.commit()
+    cursor.close()
+    connection.close()
+    print(str(datetime.timedelta(seconds=time.time()-start)))
