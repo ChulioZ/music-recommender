11a96bb8cd302c9d51ff16e7369b8097153b3358
Julian Zenker <j.zenker@gmx.net>
2018-06-03 10:48:15 +0200

    now wants the centroids as parameter instead of getting them from the db

--- src/back end/centroid_distance_getter.py
+++ src/back end/centroid_distance_getter.py
@@ -3,48 +3,25 @@ import math
 import numpy as np
 
 
-def get_centroid_distances():
-    server = 'mrd.database.windows.net'
-    database = 'mrd'
-    username = 'qaywsx'
-    password = 'w@970881'
-    connection = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER=' +
-                                server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD=' + password)
-    cursor = connection.cursor()
+def get_centroid_distances(centroids):
+    centroid_distances = {}
+    for j in range(0, len(centroids)):
+        centroid_distances[j] = {}
+        centroid_distances[j]['loudness'] = centroids[j][0]
+        centroid_distances[j]['hotttnesss'] = centroids[j][1]
+        centroid_distances[j]['tempo'] = centroids[j][2]
+        centroid_distances[j]['timeSig'] = centroids[j][3]
+        centroid_distances[j]['songkey'] = centroids[j][4]
 
-    param = "SELECT loudness,hotttnesss,tempo,timeSig,songkey,mode FROM centroids"
-    cursor.execute(param)
-    paramResults = cursor.fetchall()
-
-    centroids = {}
-    for j in range(0, 35):
-        centroids[j] = {}
-        centroids[j]['loudness'] = float(paramResults[j][0])
-        centroids[j]['hotttnesss'] = float(paramResults[j][1])
-        centroids[j]['tempo'] = float(paramResults[j][2])
-        centroids[j]['timeSig'] = float(paramResults[j][3])
-        centroids[j]['songkey'] = float(paramResults[j][4])
-        centroids[j]['mode'] = float(paramResults[j][5])
-
-    for key in ['loudness', 'hotttnesss', 'tempo', 'timeSig', 'songkey', 'mode']:
-        max_value = -float("inf")
-        min_value = float("inf")
-        for index in range(0, 35):
-            max_value = max(max_value, float(centroids[index][key]))
-            min_value = min(min_value, float(centroids[index][key]))
-        for index in range(0, 35):
-            centroids[index][key] = (
-                float(centroids[index][key]) - min_value)/(max_value - min_value) * 100
-
-    for index in range(0, 35):
+    for index in range(0, len(centroids)):
         distances = {}
-        for j in range(0, 35):
+        for j in range(0, len(centroids)):
             if index != j:
                 distance = 0
-                for key in ['loudness', 'hotttnesss', 'tempo', 'timeSig', 'songkey', 'mode']:
-                    distance += math.pow(centroids[index]
-                                         [key] - centroids[j][key], 2)
-                distances[j] = distance
-        centroids[index]['distances'] = distances
+                for key in ['loudness', 'hotttnesss', 'tempo', 'timeSig', 'songkey']:
+                    distance += math.pow(centroid_distances[index]
+                                         [key] - centroid_distances[j][key], 2)
+                distances[j] = math.sqrt(distance)
+        centroid_distances[index]['distances'] = distances
 
-    return centroids
+    return centroid_distances
