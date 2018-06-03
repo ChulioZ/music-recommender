7e066afe23727aede34e584fdad10f3781a03028
Julian Zenker <j.zenker@gmx.net>
2018-06-03 10:52:35 +0200

    is now testing kmeans

--- src/back end/main.py
+++ src/back end/main.py
@@ -1,19 +1,26 @@
 from flask import Flask, jsonify
-from db_creator import read_song_infos
-from clustering import do_kmeans
+import time
+import datetime
+from db_creator import build_db, change_db
 from recommender import recommend, test_recommend
+from kmeans_tester import test_kmeans
+from listening_count_reader import get_listening_ids
 
 app = Flask(__name__)
 
 
 @app.route('/', methods=['GET'])
 def m():
-    # read_song_infos()
-    # do_kmeans()
+    start = time.time()
+    # build_db() # if the data base has to be built from scrath
+    # change_db() # if small changes to the data base have to be made
     #ret_dict = recommend(['SOMAKIT12A58A7E292', 'SOPSAIO12A58A7AE45'])
-    #print(ret_dict)
+    # print(ret_dict)
     #ret_dict2 = test_recommend(['SOMAKIT12A58A7E292', 'SOPSAIO12A58A7AE45'], ['SOHXDYZ12A8C145925', 'SOYRSUR12A6D4FB19B'])
     #print(ret_dict2['SOHXDYZ12A8C145925'], ret_dict2['SOYRSUR12A6D4FB19B'])
+    entered_ids, test_ids_good, test_ids_bad = get_listening_ids()
+    test_kmeans(entered_ids, test_ids_good, test_ids_bad)
+    print(str(datetime.timedelta(seconds=time.time()-start)))
     return 'Test'
 
 
