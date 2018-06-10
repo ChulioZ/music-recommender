501747f55efe2e7bc8913f234a2054ba57be9b8e
Julian Zenker <j.zenker@gmx.net>
2018-06-10 15:03:53 +0200

    build db

--- src/back end/main.py
+++ src/back end/main.py
@@ -12,14 +12,14 @@ app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def m():
     start = time.time()
-    # build_db() # if the data base has to be built from scrath
-    # change_db() # if small changes to the data base have to be made
+    build_db()  # if the data base has to be built from scrath
+    # change_db() # if changes to the data base have to be made
     #ret_dict = recommend(['SOMAKIT12A58A7E292', 'SOPSAIO12A58A7AE45'])
     # print(ret_dict)
     #ret_dict2 = test_recommend(['SOMAKIT12A58A7E292', 'SOPSAIO12A58A7AE45'], ['SOHXDYZ12A8C145925', 'SOYRSUR12A6D4FB19B'])
     #print(ret_dict2['SOHXDYZ12A8C145925'], ret_dict2['SOYRSUR12A6D4FB19B'])
-    entered_ids, test_ids_good, test_ids_bad = get_listening_ids()
-    test_kmeans(entered_ids, test_ids_good, test_ids_bad)
+    #entered_ids, test_ids_good, test_ids_bad = get_listening_ids()
+    #test_kmeans(entered_ids, test_ids_good, test_ids_bad)
     print(str(datetime.timedelta(seconds=time.time()-start)))
     return 'Test'
 
