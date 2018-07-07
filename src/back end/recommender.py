6c8f8e8b520bfe72f330116c72ae9aaed0d7554c
Julian Zenker <j.zenker@gmx.net>
2018-07-07 21:59:08 +0200

    added Random Forest recommendation

--- src/back end/recommender.py
+++ src/back end/recommender.py
@@ -1,21 +1,65 @@
-from db_song_getter import get_all_songinfos, get_all_centroids
-from centroid_distance_getter import get_centroid_distances
-from clustering import do_kmeans
 import math
-import numpy as np
-from functools import reduce
-from song_dict_reader import read_song_dict_w_labels
 import operator
+from functools import reduce
+
+import numpy as np
 from numpy import linalg as LA
+from sklearn.ensemble import RandomForestClassifier
+
+from centroid_distance_getter import get_centroid_distances
+from listening_count_reader import get_listened_songs
+from song_dict_reader import read_song_dict_w_labels
 
 
 def recommend(song_dict, entered_ids, amount):
     clusters = build_song_clusters(song_dict, 200)
-    for songid in entered_ids:
+    for song_id in entered_ids:
         ids_to_give_points = [
-            sid for sid in clusters[song_dict[songid]['label']] if sid not in entered_ids]
+            sid for sid in clusters[song_dict[song_id]['label']] if sid not in entered_ids]
         song_dict = distribute_points(
-            ids_to_give_points, song_dict, songid)
+            ids_to_give_points, song_dict, song_id)
+    print_recommendation(song_dict, entered_ids, amount)
+
+
+def recommend_w_rf(song_dict, entered_ids, amount):
+    rf_ids = []
+    rf_pars = []
+    rf_targets = []
+    listened_songs = get_listened_songs(limits=[9, 24])[0]
+    for user in listened_songs.keys():
+        contains_good_song = False
+        for song_id in entered_ids:
+            contains_good_song = song_id in listened_songs[user]['good']
+        if contains_good_song:
+            for like in ['bad', 'medium', 'good']:
+                for song_id in listened_songs[user][like]:
+                    if song_id not in rf_ids:
+                        rf_ids.append(song_id)
+                        rf_pars.append([song_dict[song_id][par] for par in [
+                                       'loudness', 'hotttnesss', 'tempo', 'timeSig', 'songkey', 'mode']])
+                        rf_targets.append(like)
+                    elif (rf_targets[rf_ids.index(song_id)] == 'bad') or (rf_targets[rf_ids.index(song_id)] == 'medium' and like == 'good'):
+                        rf_targets[rf_ids.index(song_id)] = like
+    rf = RandomForestClassifier()
+    rf.fit(rf_pars, rf_targets)
+    ids = []
+    songs_to_predict = []
+    for song_id in song_dict:
+        songs_to_predict.append([song_dict[song_id][par] for par in [
+                                'loudness', 'hotttnesss', 'tempo', 'timeSig', 'songkey', 'mode']])
+        ids.append(song_id)
+    predictions = rf.predict(songs_to_predict)
+    good_indices = []
+    for i in range(0, len(predictions)):
+        if predictions[i] == 'good':
+            good_indices.append(i)
+    ids_to_give_points = [ids[index]
+                          for index in good_indices if ids[index] not in entered_ids]
+    song_dict = distribute_points(ids_to_give_points, song_dict, song_id)
+    print_recommendation(song_dict, entered_ids, amount)
+
+
+def print_recommendation(song_dict, entered_ids, amount):
     point_dict = build_point_dict(song_dict)
     print('You entered the following songs ...')
     for entered_id in entered_ids:
@@ -29,11 +73,11 @@ def recommend(song_dict, entered_ids, amount):
         point_dict.pop(best_song)
 
 
-def test_recommend(entered_ids, ids2test, cp, ncp, song_dict, centroids):
+def test_recommend(entered_ids, ids2test, song_dict, centroids):
     clusters = build_song_clusters(song_dict, len(centroids))
-    centr_distances = get_centroid_distances(centroids, cp)
-    for songid in entered_ids:
-        song_label = song_dict[songid]['label']
+    centr_distances = get_centroid_distances(centroids)
+    for song_id in entered_ids:
+        song_label = song_dict[song_id]['label']
         distances = []
         for i in range(0, len(centroids)):
             if i != song_label:
@@ -42,17 +86,17 @@ def test_recommend(entered_ids, ids2test, cp, ncp, song_dict, centroids):
         avg_distance = np.mean(distances)
         for i in range(0, len(centroids)):
             if i == song_label:
-                song_dict = distribute_test_points(
-                    list(reduce(set.intersection, map(set, [clusters[i], ids2test]))), song_dict, songid, 8, 10, cp, ncp)
+                song_dict = distribute_points(
+                    list(reduce(set.intersection, map(set, [clusters[i], ids2test]))), song_dict, song_id)
             elif centr_distances[song_label]['distances'][i] <= 0.1 * avg_distance:
-                song_dict = distribute_test_points(
-                    list(reduce(set.intersection, map(set, [clusters[i], ids2test]))), song_dict, songid, 6, 7, cp, ncp)
+                song_dict = distribute_points(
+                    list(reduce(set.intersection, map(set, [clusters[i], ids2test]))), song_dict, song_id, min_points=6, max_points=7)
             elif centr_distances[song_label]['distances'][i] <= 0.25 * avg_distance:
-                song_dict = distribute_test_points(
-                    list(reduce(set.intersection, map(set, [clusters[i], ids2test]))), song_dict, songid, 4, 5, cp, ncp)
+                song_dict = distribute_points(
+                    list(reduce(set.intersection, map(set, [clusters[i], ids2test]))), song_dict, song_id, min_points=4, max_points=5)
             elif centr_distances[song_label]['distances'][i] <= 0.5 * avg_distance:
-                song_dict = distribute_test_points(
-                    list(reduce(set.intersection, map(set, [clusters[i], ids2test]))), song_dict, songid, 2, 3, cp, ncp)
+                song_dict = distribute_points(
+                    list(reduce(set.intersection, map(set, [clusters[i], ids2test]))), song_dict, song_id, min_points=2, max_points=3)
     point_dict = build_point_dict(song_dict)
     return point_dict
 
@@ -68,29 +112,20 @@ def build_song_clusters(song_dict, count):
     return clusters
 
 
-def distribute_points(ids_to_give_points, song_dict, entered_id):
-    max_points = 10
-    min_points = 8
+def distribute_points(ids_to_give_points, song_dict, entered_id, min_points=8, max_points=10):
+    if not ids_to_give_points:
+        return song_dict
     distances = []
-    ncp_distances = []
-    cp = ['timeSig', 'songkey', 'mode']
-    ncp = ['loudness', 'tempo']
+    pars = ['timeSig', 'songkey', 'mode', 'loudness', 'tempo']
     for sid in ids_to_give_points:
         if sid is not entered_id:
             par_distances = [song_dict[entered_id][key] -
-                             song_dict[sid][key] for key in cp]
+                             song_dict[sid][key] for key in pars]
             distance = LA.norm(par_distances, 2)
             song_dict[sid]['distance'] = distance
             distances.append(distance)
-            ncp_par_distances = [song_dict[entered_id][key] -
-                                 song_dict[sid][key] for key in ncp]
-            ncp_distance = LA.norm(ncp_par_distances, 2)
-            song_dict[sid]['ncpdistance'] = ncp_distance
-            ncp_distances.append(ncp_distance)
     max_distance = max(distances)
     min_distance = min(distances)
-    max_ncp_distance = max(ncp_distances)
-    min_ncp_distance = min(ncp_distances)
     for sid in ids_to_give_points:
         if(max_distance == min_distance):
             points = max_points
@@ -98,55 +133,8 @@ def distribute_points(ids_to_give_points, song_dict, entered_id):
             diff = max_distance - song_dict[sid]['distance']
             points = diff / (max_distance - min_distance) * \
                 (max_points - min_points) + min_points
-        points *= len(cp)/(len(cp) + len(ncp))
-        if(max_ncp_distance == min_ncp_distance):
-            ncp_points = max_points
-        else:
-            diff = max_ncp_distance - song_dict[sid]['ncpdistance']
-            ncp_points = diff / (max_ncp_distance - min_ncp_distance) * \
-                (max_points - min_points) + min_points
-        ncp_points *= len(ncp)/(len(cp) + len(ncp))
         song_dict[sid]['points'] = max(
-            song_dict[sid]['points'], points + ncp_points)
-    return song_dict
-
-
-def distribute_test_points(ids_in_cluster, song_dict, songid, min_points, max_points, cp, ncp):
-    distances = []
-    ncp_distances = []
-    for key in ncp:
-        ncp_distances.append([])
-    for sid in ids_in_cluster:
-        if sid is not songid:
-            par_distances = [song_dict[songid][key] -
-                             song_dict[sid][key] for key in cp]
-            distance = LA.norm(par_distances, 2)
-            song_dict[sid]['distance'] = distance
-            distances.append(distance)
-            for i in range(0, len(ncp)):
-                ncp_distances[i].extend(
-                    [math.pow(song_dict[songid][ncp[i]] - song_dict[sid][ncp[i]], 2)])
-    max_distance = max(distances)
-    min_distance = min(distances)
-    ncp_distance_means = [np.mean(index) for index in ncp_distances]
-    for sid in ids_in_cluster:
-        if(max_distance == min_distance):
-            points = max_points
-        else:
-            diff = max_distance - song_dict[sid]['distance']
-            points = diff / (max_distance - min_distance) * \
-                (max_points - min_points) + min_points
-        points *= len(cp)/(len(cp) + len(ncp))
-        for i in range(0, len(ncp)):
-            dist = math.pow(song_dict[songid]
-                            [ncp[i]] - song_dict[sid][ncp[i]], 2)
-            if dist == 0:
-                points += 1/(len(cp) + len(ncp))*max_points
-            elif dist <= 0.1 * ncp_distance_means[i]:
-                points += 1/(2 * (len(cp) + len(ncp)))*max_points
-            elif dist <= 0.5 * ncp_distance_means[i]:
-                points += 1/(4 * (len(cp) + len(ncp)))*max_points
-        song_dict[sid]['points'] = max(song_dict[sid]['points'], points)
+            song_dict[sid]['points'], points)
     return song_dict
 
 
